fe7d5958d4dbe2ab23c8e906784fb23efbc6330e
nicholaiTukanov <nicholaitukanov@gmail.com>
2023-11-29 13:43:54 -0500

    Add training

--- qcnns.py
+++ qcnns.py
@@ -1,7 +1,19 @@
-import qiskit
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
+from IPython.display import clear_output
+
+import qiskit
+from qiskit.quantum_info import SparsePauliOp
+from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
+from qiskit_machine_learning.neural_networks import EstimatorQNN
+
+import qiskit_algorithms.optimizers as qiskit_optimizers
+
+from sklearn.model_selection import train_test_split
+
+from img_gen import generate_dataset
 
 # using qiskit tutorial to build a convolutional quantum circuit
 def conv_circuit(params):
@@ -62,7 +74,7 @@ def pool_layer(sources, sinks, param_prefix):
     qc.append(qc_inst, range(num_qubits))
     return qc
 
-def make_qccn(num_qubits, name, feature_map):
+def make_qcnn(num_qubits, name, feature_map):
     # assumes num_qubits is a power of 2
     total_layers = math.log2(num_qubits)
     qcnn = qiskit.QuantumCircuit(num_qubits, name=name)
@@ -88,9 +100,82 @@ def make_qccn(num_qubits, name, feature_map):
     circ = qiskit.QuantumCircuit(num_qubits)
     circ.compose(feature_map, range(num_qubits), inplace=True)
     circ.compose(qcnn, range(num_qubits), inplace=True)
-    return circ
     
-qubits = 8
-qcnn = make_qccn(qubits, "test", qiskit.circuit.library.ZFeatureMap(qubits))
-qcnn.draw("mpl", 1, "qcnn.png")
+    observable = SparsePauliOp.from_list([("Z" + "I" * 7, 1)])
+
+    # we decompose the circuit for the QNN to avoid additional data copying
+    qcnn_estimator = EstimatorQNN(
+        circuit=circ.decompose(),
+        observables=observable,
+        input_params=feature_map.parameters,
+        weight_params=qcnn.parameters,
+    )
+    
+    return circ, qcnn_estimator
+    
+    
+objective_func_vals = []
+plt.title("Objective function value against iteration")
+plt.xlabel("Iteration")
+plt.ylabel("Objective function value")
+hl, = plt.plot([], [])
+
+def callback_graph(weights, obj_func_eval):
+    # clear_output(wait=True)
+    # objective_func_vals.append(obj_fu nc_eval)
+    # plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    hl.set_xdata(np.append(hl.get_xdata(), len(objective_func_vals)))
+    hl.set_ydata(np.append(hl.get_ydata(), obj_func_eval))
+    plt.draw()
+    
+# def plot_graph():
+#     plt.title("Objective function value against iteration")
+#     plt.xlabel("Iteration")
+#     plt.ylabel("Objective function value")
+#     plt.plot(range(len(objective_func_vals)), objective_func_vals)
+#     plt.show()
+    
+def train_qcnn(qnn_estimator, optimizer, callback_fn, data, labels):
+    classifier = NeuralNetworkClassifier(
+        qnn_estimator,
+        optimizer=optimizer,
+        callback=callback_fn
+    )
+    plt.rcParams["figure.figsize"] = (12, 6)
+    plt.show(block = False)
+    classifier.fit(np.asarray(data), np.asarray(labels))
+    print(f"Accuracy from the train data : {np.round(100 * classifier.score(data, labels), 2)}%")
+    
+    return classifier
+
+if __name__ == "__main__":
+    
+    max_qubits = 8
+    feat_map = qiskit.circuit.library.ZFeatureMap(max_qubits)
+    circ_qcnn, qcnn_estimator = make_qcnn(max_qubits, "QCNN", feat_map)
+    # circ_qcnn.draw("mpl", 1, "qcnn.png")
+    
+    image_h, image_w = 4, 2
+    assert(image_h * image_w == max_qubits)
+    data, labels = generate_dataset(100, image_h, image_w, 2)
+    train_data, test_data, train_labels, test_labels = train_test_split(
+        data, labels, test_size=0.3
+    )
+    
+    # uncomment to print the first 4 images
+    # fig, ax = plt.subplots(2, 2, figsize=(10, 6), subplot_kw={"xticks": [], "yticks": []})
+    # for i in range(4):
+    #     ax[i // 2, i % 2].imshow(
+    #         train_data[i].reshape(2, 4),  # Change back to 2 by 4
+    #         aspect="equal",
+    #     )
+    # plt.subplots_adjust(wspace=0.1, hspace=0.025)
+    # plt.show()
+    
+    classifier = train_qcnn(
+        qcnn_estimator, 
+        qiskit_optimizers.COBYLA(maxiter=100), 
+        callback_graph, 
+        train_data, train_labels
+    )
     
