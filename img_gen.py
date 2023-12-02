784f6103509b1b8cc88f01ab9e9b99fb03bf193c
Kevinni2001 <yni61@gatech.edu>
2023-12-01 23:36:47 -0500

    Update img_gen.py
    Added function to take MNIST dataset and shrink the image size by processing the images with a CNN pooling layer

--- img_gen.py
+++ img_gen.py
@@ -1,6 +1,13 @@
 import numpy as np
 from qiskit.utils import algorithm_globals
 
+## import for MNIST data 
+from scipy.ndimage import uniform_filter
+import torch
+from torch import cat, no_grad, manual_seed
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
 def generate_dataset(num_images, image_height, image_width, stroke_length):
     images = []
     labels = []
@@ -64,3 +71,56 @@ def generate_dataset(num_images, image_height, image_width, stroke_length):
             if images[-1][i] == 0:
                 images[-1][i] = algorithm_globals.random.uniform(0, np.pi / 4)
     return images, labels
+
+def import_MNIST_Torch(num_samples, final_img_size):
+    # extract the dataset from pytorch API
+    # Set train shuffle seed (for reproducibility)
+    manual_seed(42)
+
+    batch_size = 1
+    n_samples = num_samples  # We will concentrate on the first num_samples samples of each pattern
+
+    # Use pre-defined torchvision function to load MNIST train data
+    X_train = datasets.MNIST(
+        root="./data", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()])
+    )
+
+    # Filter out labels (originally 0-9), leaving only labels 0 and 1
+    idx = np.append(
+        np.where(X_train.targets == 0)[0][:n_samples], np.where(X_train.targets == 1)[0][:n_samples]
+    )
+    X_train.data = X_train.data[idx]
+    X_train.targets = X_train.targets[idx]
+
+    # process the data
+    # Define the blur kernel size
+    original_size = 28
+    target_size = final_img_size
+    kernel_size = original_size // target_size
+
+    # data structure to store output
+    out_data = np.empty((n_samples*2, target_size, target_size))
+    out_labels = X_train.targets.numpy()
+
+    for data_idx, data in enumerate(X_train.data):
+        # reshape and cast to numpy array
+        img = data.reshape(28,28).numpy()
+
+        # Apply uniform blur to the image (traditional CNN method)
+        blurred_img = uniform_filter(img, size=kernel_size)
+
+        # Subsample the image by selecting every 'kernel_size'-th pixel
+        # shrunken_img = blurred_img[::kernel_size, ::kernel_size]
+        shrunken_img = blurred_img[:target_size * kernel_size, :target_size * kernel_size]
+        shrunken_img = shrunken_img.reshape((target_size, kernel_size, target_size, kernel_size)).mean(3).mean(1)
+
+        # convert back to tensor and store it back
+        # tensor_img = torch.from_numpy(shrunken_img)
+
+        # add it back to X_train.data
+        out_data[data_idx] = shrunken_img
+
+    # Define torch dataloader with filtered data
+    train_loader = DataLoader(X_train, batch_size=batch_size, shuffle=True)
+
+    return out_data, out_labels
